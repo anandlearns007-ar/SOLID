a150b297f1187e295570e75e2554568d83df8fcb
USER <USER@DESKTOP-VQBCHDK>
2026-05-19 00:58:41 +0530

    Add project files.

---
+++ SOLID/SRPbad.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID
+{
+    public class OrderManager
+    {
+        public void CreateOrder()
+        {
+            Console.WriteLine("Your order has been placed");
+        }
+
+        public void Payment()
+        {
+            Console.WriteLine("Your payment has been completed");
+        }
+
+        public void Email()
+        {
+            Console.WriteLine("Your payment is complete, please track your order");
+        }
+
+        public void Invoice()
+        {
+            Console.WriteLine("Here is invoice for your current order");
+        }
+    }
+}
+
---
+++ SOLID/SRPgood.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID
+{
+    public class OrderService
+    {
+        public void Order()
+        {
+            Console.WriteLine("Your order has been placed");
+        }
+    }
+
+    public class PaymentService
+    {
+        public void Payment()
+        {
+            Console.WriteLine("Your payment has been received");
+        }
+    }
+
+    public class EmailService
+    {
+        public void Email()
+        {
+            Console.WriteLine("Your order is placed, please track your order");
+        }
+    }
+
+    public class InvoiceService()
+    {
+        public void Invoice()
+        {
+            Console.WriteLine("Here is your invoice for the order placed");
+        }
+    }
+}
