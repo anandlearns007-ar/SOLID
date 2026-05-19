1a026b247c8dd178d642b172e4084228f36010ba
Anand Raj <anandlearns007@gmail.com>
2026-05-20 00:22:52 +0530

    LSV bad example

---
+++ SOLID/Services/LSVbad.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID.Services
+{
+    public class Vehicle
+    {
+        public virtual void StartEngine()
+        {
+            Console.WriteLine("Vehicle Started");
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public override void StartEngine()
+        {
+            Console.WriteLine("Car started");
+        }
+    }
+
+    public class Cycle : Vehicle
+    {
+        public override void StartEngine()
+        {
+            Console.WriteLine("Car started");
+        }
+    }
+}
